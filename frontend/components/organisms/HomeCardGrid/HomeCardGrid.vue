868eed03698d447241e49e136ffe99322f324786
Tara Matthew <taramatthew.93@gmail.com>
2021-08-09 21:26:02 +0100

    Add home card grid organism

---
+++ frontend/components/organisms/HomeCardGrid/HomeCardGrid.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div v-for="(card, index) in homeCards" :key="index">
+            <home-card :cardItems="card.items" :title="card.title"></home-card>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from '@nuxtjs/composition-api';
+import HomeCard from '~/components/molecules/HomeCard/HomeCard.vue';
+
+export default defineComponent({
+    components: { HomeCard },
+    props: {
+        homeCards: {}
+    },
+    setup (props: Data) {
+    }
+});
+
+</script>
